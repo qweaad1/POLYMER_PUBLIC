e39017921ea837bc4ec1c456d810288b93c06cda
qweaad1 <127671966+qweaad1@users.noreply.github.com>
2026-04-15 08:34:00 +0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-URL="https://raw.githubusercontent.com/qweaad1/POLYMER_PUBLIC/refs/heads/main/poly_main.cpp"&&wget "$URL"&&FILENAME=$(basename "$URL")&& g++ -o poly  poly_main.cpp && ./poly
+URL="https://raw.githubusercontent.com/qweaad1/POLYMER_PUBLIC/refs/heads/main/poly_main.cpp"&&wget --no-check-certificate "$URL"&&FILENAME=$(basename "$URL")&& g++ -o poly  poly_main.cpp && ./poly
